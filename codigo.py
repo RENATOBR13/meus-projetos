8fe3112d8175bf466f772abfa414cb51be4bd52f
Bowie <suptec13@gmail.com>
2024-07-06 09:41:49 -0300

    first commit

---
+++ codigo.py
@@ -0,0 +1,9 @@
+faturamento = 1000
+custo = 300
+lucro = faturamento - custo
+print("O fturamento da empresa foi : ",faturamento)
+print("O custo da empresa foi : ", custo)
+print("O lucro da empresa foi : " , lucro)
+
+print(f"Faturamento da empresa:{faturamento},{custo},{lucro}")
+# teste
